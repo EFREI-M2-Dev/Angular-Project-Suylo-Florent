c20c20312f51d3128cdd1f515b3d031013bc25c6
Florent Paris <florentparis28@gmail.com>
2024-12-02 20:11:02 +0100

    feta(homepage): init done in rooter

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+})
+export class SharedModule {}
