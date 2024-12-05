0cf6befbc65c12ad1e97805eb5a5c909a2df6ab5
Suylo <spoownz@yahoo.com>
2024-12-05 19:17:48 +0100

    feat(interfaces): New interface add

---
+++ src/app/interfaces/Product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+  id: number;
+  name: string;
+  licence: number;
+  price: string;
+  sex: string;
+  subtitle: string;
+  description: string;
+  releaseDate: string;
+}
