992a41e88fd1212a300372ef7dfc1ca55f3a071d
Suylo <spoownz@yahoo.com>
2024-12-05 17:43:44 +0100

    feat(header): header add

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  exports: [
+    HeaderComponent
+  ]
 })
 export class SharedModule {}
