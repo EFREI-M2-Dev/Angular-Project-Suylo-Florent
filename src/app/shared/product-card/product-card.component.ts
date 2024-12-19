78fd2f28c06d396dc12d085ac945937cc7af6ecc
Suylo <spoownz@yahoo.com>
2024-12-19 18:38:56 +0100

    feat(product-card): add to cart on card

--- src/app/shared/product-card/product-card.component.ts
+++ src/app/shared/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output } from '@angular/core';
 import { ProductModel } from '../models/product.model';
+import {CartService} from "../../core/services/cart.service";
 
 @Component({
   selector: 'app-product-card',
@@ -11,11 +12,28 @@ export class ProductCardComponent {
   @Input() onEdit!: () => void;
   @Input() canEdit!: boolean;
 
+  constructor(private cartService: CartService) {
+  }
+
   handleEdit(event: MouseEvent): void {
     event.preventDefault();
     event.stopPropagation();
     if (this.onEdit) {
       this.onEdit();
     }
+  }
+
+  addToCart(event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.cartService.addToCart(this.product).subscribe(
+      (response) => {
+        console.log('Produit ajouté au panier', response);
+        document.dispatchEvent(new Event('addToCart'));
+      },
+      (error) => {
+        console.error("Erreur lors de l'ajout au panier", error);
+      }
+    );
   }
 }
