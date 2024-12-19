3ff9caef170493ba658f4675b15b221637c9826f
Florent Paris <florentparis28@gmail.com>
2024-12-19 12:54:22 +0100

    feat(product-form): wip

--- src/app/features/form-product/form-product.component.ts
+++ src/app/features/form-product/form-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/core/services/product.service';
 
 @Component({
   selector: 'app-form-product',
@@ -7,17 +9,44 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./form-product.component.scss'],
 })
 export class FormProductComponent implements OnInit {
+  productForm!: FormGroup;
   productId: string | null = null;
+  productData: any = {};
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.paramMap.get('id');
 
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required]],
+      description: [''],
+      price: ['', [Validators.required]],
+    });
+
     if (this.productId) {
-      console.log(`Edit product with ID: ${this.productId}`);
+      this.getProductData();
     } else {
       console.log('Create new product');
     }
+  }
+
+  getProductData(): void {
+    this.productService.getProductById(this.productId!).subscribe((data) => {
+      console.log(data);
+      this.productData = data;
+
+      this.productForm.patchValue({
+        name: this.productData.name,
+      });
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.productForm.value);
   }
 }
